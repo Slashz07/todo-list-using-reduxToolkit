e7f913185a3d133cc4ffa35b5e201fb87915574f
Slashz07 <sumitforsaken76248@gmail.com>
2024-06-04 18:20:06 +0530

    components added with todo component being in loop of array of todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import AddTodo from "./components/AddTodo.jsx"
+import Todo from "./components/Todo.jsx"
+import { useSelector } from "react-redux"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const todos=useSelector((state)=>state.todos)
 
   return (
-    <>
-     <h1>Hello there</h1>
-    </>
+      <div>
+      <AddTodo />
+    <div>Todos</div>
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            key={todo.id}
+          >
+       <Todo todoObj={todo} />
+
+          </li>
+        ))}
+        </ul>
+        </div>
   )
 }
 
