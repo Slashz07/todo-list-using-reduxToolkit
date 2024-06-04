fd95ce0d77a051f6f84aa452a638f017dc3812f5
Slashz07 <sumitforsaken76248@gmail.com>
2024-06-04 18:21:11 +0530

    .

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
-import configureStore from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import todoReducer from "../features/todo/todoSlice.js"
 
-export const store = configureStore({
-  reducer:todoReducer
+export const Store = configureStore({
+  reducer: todoReducer
 })
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -4,7 +4,7 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 // creating initial state of store as to how it will appear in the begining--->
 
 const initialState = {
-  todos: [{ id: 1, des: "this is first todo" }]
+  todos: [{ id: 1, text: "this is first todo" }]
 }
 
 // creating slice-->
@@ -14,18 +14,18 @@ export const todoSlice = createSlice({
   reducers: {
     addTodo: (state, action) => {
       const todo = {
-        id:nanoid(),
-        des:action.payload.text
+        id: nanoid(),
+        text: action.payload
       }
       state.todos.push(todo)
     },
-    removeTodo: (state,action) => {
-      state.todos=state.todos.filter((todo)=>todo.id!=action.payload)
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id != action.payload)
     },
     updateTodo: (state, action) => {
       state.todos = state.todos.map((todo) => {
         if (todo.id === action.payload) {
-          todo.des=action.payload
+          todo.text = action.payload.text
         }
         return todo
       })
